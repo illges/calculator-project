575f39f0bfd3f7ec64544d198cd56cda18ade660
illges <brown1ei.eb@gmail.com>
2018-12-05 10:43:43 -0500

    Initial commit for Calculator webpage.

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+	return a + b
+}
+
+function subtract(a, b) {
+	return a - b
+}
+
+function multiply(a, b) {
+	return a * b
+}
+
+function divide(a, b) {
+	return a / b
+}
+
+
+function display(e) {
+	
+
+	return document.getElementById('display').innerHTML = `${e.target.innerHTML}`;
+	//return console.log(e.target.innerHTML);
+
+}
+
+const buttons = Array.from(document.querySelectorAll('button'));
+buttons.forEach(btn => btn.addEventListener('click', e => display(e)));
+
+
